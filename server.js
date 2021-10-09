8019c427133ae7254753b83a542092e44eefa89e
shpenguin <39072247+shpenguin@users.noreply.github.com>
2021-10-09 15:50:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  log: [drillSchema]
+});
+
+const drillSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: {type: Date, required: true }
+});
+
+const Users = mongoose.model('user', userSchema);
+const Drills = mongoose.model('drill', drillSchema);
 
 app.use(cors())
 app.use(express.static('public'))
